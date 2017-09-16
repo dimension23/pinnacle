cb4efe9f0daf170858f94858c6b7a2f25c939469
Prashant Patel <prashant1139@gmail.com>
2017-09-16 23:07:06 +0100

    datatable

--- public/javascripts/dropzone.options.js
+++ public/javascripts/dropzone.options.js
@@ -2,7 +2,7 @@ Dropzone.options.dropzoneLeft = {
   paramName: 'leftFile',
   maxFilesize: 2,
   maxFiles: 1,
-  dictDefaultMessage: 'Drag an xliff here to upload, or click to select one',
+  dictDefaultMessage: 'Drop <b>machine translated</b> file here, or click to select one',
   acceptedFiles: 'text/xml',
   init: function() {
     this.on('success', function(file, resp){
@@ -15,7 +15,7 @@ Dropzone.options.dropzoneRight = {
   paramName: 'rightFile',
   maxFilesize: 2,
   maxFiles: 1,
-  dictDefaultMessage: 'Drag an xliff here to upload, or click to select one',
+  dictDefaultMessage: 'Drop <b>post edited</b> file here, or click to select one',
   acceptedFiles: 'text/xml',
   init: function() {
     this.on('success', function(file, resp){
